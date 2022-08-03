ed3ab715e97132666a4d9b2663b7ff7d34497be7
Nhujarski <52231438+Nhujarski@users.noreply.github.com>
2022-08-03 12:32:53 -0700

    added functionality

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./clock.css";
 
 const Clock = () => {
-  return <div className='clock'></div>;
+  const [time, setTime] = useState(new Date());
+
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+
+  const getTime = () => {
+    setTime(new Date());
+    console.log("time in intervl >>> ", time);
+  };
+  setInterval(getTime, 60000);
+
+  useEffect(() => {
+    hours = time.getHours();
+    minutes = time.getMinutes();
+  }, [time]);
+
+  return (
+    <div className='clock'>
+      <div className='clock-face'>
+        <p className='clock-display'>
+          {hours} : {minutes}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Clock;
